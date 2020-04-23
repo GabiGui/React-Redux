bb43c1a5f65cd60623cc61fb80645661bea7e658
Gabi <gaaabsilva@hotmail.com>
2020-04-22 22:58:56 -0300

    propagar propriedades
    sobre propagar propriedades

---
+++ exercicios-react/src/components/Familia.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        {React.cloneElement(props.children, {...props})}
+        {/* {React.cloneElement(props.children, {sobrenome: props.sobrenome})} */}
+        {props.children}
+    </div>
+)
--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -2,13 +2,18 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 // import { ComponenteA, ComponenteB } from './components/DoisComponentes';
 /* import MultiElementos from './components/MultiElementos'; */
-import FamiliaSilva from './components/FamiliaSilva';
+//import FamiliaSilva from './components/FamiliaSilva';
+import Familia from './components/Familia';
+import Membro from './components/Membro';
 
 const elemento = document.getElementById('root');
 
 ReactDOM.render(
     <div>
-        <FamiliaSilva/>
+        <Familia sobrenome="Silva" saudacao="oi">
+            <Membro nome="André"/>
+        </Familia>
+        {/* <FamiliaSilva/> */}
         {/* <ComponenteA valor="e aí"/>
         <ComponenteB valor="olá"/> */}
         {/* <MultiElementos/> */}
